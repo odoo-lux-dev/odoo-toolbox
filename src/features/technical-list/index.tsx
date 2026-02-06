4870f8634117159162161e5b7b3111068cf3785a
thcl-odoo <thcl@odoo.com>
2026-02-06 23:18:27 +0100

    [FIX] Exclude Technical Sidebar on specific pages
    Add a new function to specify rules for disabling the technical sidebar
    on certain pages/in certain specific use cases.
    
    Currently, it rejects the document signature page, but the function is
    generic and can easily be enhanced with other rules.
    
    Fix #40

--- src/features/technical-list/index.tsx
+++ src/features/technical-list/index.tsx
@@ -7,12 +7,26 @@ import technicalListStyles from "./technical-list.css?inline";
 let containerElement: HTMLDivElement | null = null;
 let shadowRoot: ShadowRoot | null = null;
 
+const technicalListExclusionRules = [
+    // Prevents showing the technical list on sign page
+    () => document.body?.classList.contains("o_sign_document_body") ?? false,
+];
+
+const shouldExcludeTechnicalList = () =>
+    technicalListExclusionRules.some((rule) => rule());
+
 export const initTechnicalList = (): void => {
     try {
         const showTechnicalList = getShowTechnicalList() === "true";
         const odooToolboxTheme = getOdooToolboxTheme();
+        const shouldExclude = shouldExcludeTechnicalList();
         // Only initialize if not already present and on Odoo page
-        if (showTechnicalList && !containerElement && window.odoo) {
+        if (
+            showTechnicalList &&
+            !shouldExclude &&
+            !containerElement &&
+            window.odoo
+        ) {
             containerElement = document.createElement("div");
             containerElement.id = "x-odoo-technical-list-shadow-host";
             document.body.appendChild(containerElement);
